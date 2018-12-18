18fada6faba364cd8bf8246c150434d26e4aa1ed
fernando-ts <torresinc7@mesoamericana.edu.gt>
2018-12-18 17:22:37 +0100

    refactor: Create the model logic working properly

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const model = {
-
+   //-> Array containing all the paths of the cats images 
+   imgsOfCatsArr: ['img/realCat1.jpg', 'img/realCat2.jpg', 'img/realCat3.jpg', 'img/realCat4.jpg', 'img/realCat5.jpg'],
+   //-> Array containing the names of cats
+   namesOfCats: ['Cat 1', 'Cat 2', 'Cat 3', 'Cat 4', 'Cat 5'],
+   //-> Object that stores the times an image is clicked 
+   counts : { counter1: 0, counter2: 0, counter3: 0, counter4: 0, counter5: 0 },
 };
 
 const octopus = {
@@ -17,11 +22,9 @@ const listOfCats = document.querySelector('#cats-container');
 const imgContainer = document.querySelector('.img-container');
 
 
-//-> Array containing all the paths of the cats images 
-const imgsOfCatsArr = ['img/realCat1.jpg', 'img/realCat2.jpg', 'img/realCat3.jpg', 'img/realCat4.jpg', 'img/realCat5.jpg'];
 
-//-> Array containing the names of cats
-const namesOfCats = ['Cat 1', 'Cat 2', 'Cat 3', 'Cat 4', 'Cat 5'];
+
+
 
 //-> Variable for the document fragment
 const docFrag = document.createDocumentFragment();
@@ -31,10 +34,10 @@ let indexOfCat = 0;
 
 
 //-> Create  a list of cat names in the DOM 
-for (const name of namesOfCats) {
+for (const name of model.namesOfCats) {
    let link = document.createElement('a');
    link.textContent = name;
-   link.href = imgsOfCatsArr[indexOfCat];  
+   link.href = model.imgsOfCatsArr[indexOfCat];  
    docFrag.appendChild(link);
    indexOfCat++;
    listenForClicksOnName(link, name);   
@@ -101,40 +104,39 @@ function listenForClicksOnImg() {
 } 
 
 
-//-> Object that stores the times an image is clicked 
-const counts = { counter1: 0, counter2: 0, counter3: 0, counter4: 0, counter5: 0 };
+
 //-> Funtion that handles the count for each click on a specific image 
 function handleCounterOfClicks(imgTitle) {
    //-> Select the elements where the number of clicks will be displayed
    const countsContainer = document.querySelector('#counter-clicks');
    switch (imgTitle) {
       case 'Cat 1': {
-         counts.counter1 +=1;
-         countsContainer.textContent = counts.counter1;
+         model.counts.counter1 +=1;
+         countsContainer.textContent = model.counts.counter1;
          break;
       }
          
       case 'Cat 2': {
-         counts.counter2 += 1;
-         countsContainer.textContent = counts.counter2;
+         model.counts.counter2 += 1;
+         countsContainer.textContent = model.counts.counter2;
          break;
       }
  
       case 'Cat 3': {
-         counts.counter3 += 1;
-         countsContainer.textContent = counts.counter3;
+         model.counts.counter3 += 1;
+         countsContainer.textContent = model.counts.counter3;
          break;
       }
   
       case 'Cat 4': {
-         counts.counter4 += 1;
-         countsContainer.textContent = counts.counter4;
+         model.counts.counter4 += 1;
+         countsContainer.textContent = model.counts.counter4;
          break;
       }
 
       case 'Cat 5': {
-         counts.counter5 += 1;
-         countsContainer.textContent = counts.counter5;
+         model.counts.counter5 += 1;
+         countsContainer.textContent = model.counts.counter5;
          break;
       }    
    }
